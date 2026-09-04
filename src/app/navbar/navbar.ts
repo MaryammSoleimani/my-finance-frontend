b0a7e8d8b7a74c52f87ae3ba682d2d10075bbc9f
Maryam Soleimani <maryamsoleimanii2003@gmail.com>
2026-09-04 18:30:39 +0330

    add notification components

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,20 +1,59 @@
-import { Component, AfterViewInit } from '@angular/core';
-import { RouterLink, RouterLinkActive, Router } from '@angular/router';
+
+import { Component, AfterViewInit, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive, Router, NavigationEnd } from '@angular/router';
 import { AuthService } from '../services/auth.services';
 import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
+import { NotificationService } from '../services/notification.service';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive,CommonModule],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './navbar.html',
   styleUrls: ['./navbar.css'],
 })
-export class Navbar implements AfterViewInit {
+export class Navbar implements AfterViewInit, OnInit {
   indicatorLeft = 0;
   indicatorWidth = 0;
+  unreadCount: number = 0;
+  notifications: any[] = [];
+  showNotifications: boolean = false;
+  constructor(private authService: AuthService,
+              private router: Router, private notificationService: NotificationService) {}
 
-  constructor(private authService: AuthService, private router: Router) {}
+  ngOnInit() {
+    this.loadNotifications();
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      setTimeout(() => this.syncIndicator(), 100);
+    });
+  }
+
+  loadNotifications() {
+    this.notificationService.getNotifications().subscribe({
+      next: (data) => {
+        this.notifications = data;
+        this.unreadCount = data.filter(n => !n.is_read).length;
+      },
+      error: (err) => console.error(err)
+    });
+  }
+   toggleNotifications() {
+    this.showNotifications = !this.showNotifications;
+    if (this.showNotifications) {
+      this.loadNotifications();
+    }
+  }
+
+  markAllAsRead() {
+    this.notificationService.markAllAsRead().subscribe({
+      next: () => this.loadNotifications(),
+      error: (err) => console.error(err)
+    });
+  }
 
   moveIndicator(event: MouseEvent) {
     const element = event.currentTarget as HTMLElement;
@@ -42,6 +81,6 @@ export class Navbar implements AfterViewInit {
   }
 
   get isUserLoggedIn(): boolean {
-  return this.authService.isLoggedIn();
-}
+    return this.authService.isLoggedIn();
+  }
 }
